aae5828fa39b8e2242db4d69369d861f4973dd66
RationalStudent <shafqatameen316@gmail.com>
2025-02-19 22:06:00 +0530

    used the yolov8s.pt model

--- detectimg.py
+++ detectimg.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
-import cv2
+import cv2 as cv
 import matplotlib.pyplot as plt
 
 # Load the pre-trained YOLOv8 model
-model = YOLO("yolov8n.pt")  # 'n' stands for nano (small and fast)
+model = YOLO("yolov8s.pt")  # 'n' stands for nano (small and fast)
 
-# Load an image
+# Load an 
 image_path = "data/img/000031.png"  # Change to your image path
-image = cv2.imread(image_path)
+image = cv.imread(image_path)
 
 # Run object detection
 results = model(image)
